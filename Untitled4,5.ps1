fad21ef4756073f3ebd3aa2a97b4df6760896b3b
Noah Maywar <160625130+MaywarDeChamplain@users.noreply.github.com>
2024-09-17 23:59:19 -0400

    Add files via upload

---
+++ Untitled4,5.ps1
@@ -0,0 +1,25 @@
+﻿. ($PSScriptRoot + "\Untitled3.ps1")
+
+$loginouts = GetLogInOut(10)
+$shutdowns = GetShutDown(10)
+
+Write-Host "Seperater-----"
+$loginouts | Format-Table
+Write-Host "Seperater-----"
+$shutdowns | Format-Table
+
+$loginouts = GetLogInOut(25)
+$shutdowns = GetShutDown(25)
+
+Write-Host "Seperater-----"
+$loginouts | Format-Table
+Write-Host "Seperater-----"
+$shutdowns | Format-Table
+
+$loginouts = GetLogInOut(30)
+$shutdowns = GetShutDown(30)
+
+Write-Host "Seperater-----"
+$loginouts | Format-Table
+Write-Host "Seperater-----"
+$shutdowns | Format-Table
