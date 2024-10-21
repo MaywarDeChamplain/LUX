db14e5aa19f42aa15895341463fd76bdf0b460ba
Noah Maywar <160625130+MaywarDeChamplain@users.noreply.github.com>
2024-10-21 14:32:54 -0400

    Add files via upload

---
+++ Scheduled Email Reports of At Risk Users/MainScheduledEmailReports.ps1
@@ -0,0 +1,10 @@
+﻿. "$PSScriptRoot\Scheduler.ps1"
+. "$PSScriptRoot\Users.ps1"
+. "$PSScriptRoot\Configuration.ps1"
+. "$PSScriptRoot\Email.ps1"
+. "$PSScriptRoot\EventLogs.ps1"
+
+$configuration = readConfiguration
+$Failed = getAtRiskUsers $configuration.Days
+SendAlertEmail ($failed | Format-Table | Out-String)
+ChooseTimeToRun($configuration.ExecutionTime)
